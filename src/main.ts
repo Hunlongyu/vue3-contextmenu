103051d4e506e99c746ac297b2b13d6a72e84fdc
Hunlongyu <hunlongyu@gmail.com>
2021-01-06 12:11:01 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import contextmenu from './index'
-// import contextmenu from 'vue3-contextmenu'
-// import 'vue3-contextmenu/dist/vue3-contextmenu.css'
+// import contextmenu from './index'
+import contextmenu from 'vue3-contextmenu'
+import 'vue3-contextmenu/dist/vue3-contextmenu.css'
 const app = createApp(App)
 app.use(contextmenu)
 app.mount('#app')
