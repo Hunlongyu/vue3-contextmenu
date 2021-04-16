e77f29272ee1912aba79dd6197a99afec6cc1c2c
Xhofe <i@nn.ci>
2021-04-16 20:11:10 +0800

    页面滚动之后的位置问题

--- src/components/directive.ts
+++ src/components/directive.ts
@@ -3,7 +3,7 @@ import { DirectiveBinding } from 'vue'
 const onMounted = (el: HTMLElement, binding: DirectiveBinding) => {
   el.addEventListener('contextmenu', e => {
     e.preventDefault()
-    bus.emit('add-contextmenu', { x: e.pageX, y: e.pageY, value: binding.value })
+    bus.emit('add-contextmenu', { x: e.clientX, y: e.clientY, value: binding.value })
   })
   document.addEventListener('click', () => {
     bus.emit('hide-contextmenu')
