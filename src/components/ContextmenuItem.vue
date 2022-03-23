15dc96d2ecf3e4836e8e5f06c9ca206f25237d0d
Hunlongyu <hunlongyu@gmail.com>
2022-03-23 19:34:58 +0800

    feat: add item and submenu

---
+++ src/components/ContextmenuItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="contextmenu-item">
+    <div class="icon">
+      <slot name="icon" />
+    </div>
+    <div class="label">
+      <slot name="label" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+const props = defineProps({
+  icon: String,
+  disabled: Boolean,
+  divider: {
+    type: Boolean,
+    default: false
+  }
+})
+
+onMounted(() => {
+  console.log('=== props ===', props)
+})
+
+</script>
---
+++ src/components/ContextmenuSubmenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="contextmenu-submenu">
+    <div class="icon">
+      <slot name="icon" />
+    </div>
+    <div class="label">
+      <slot name="label" />
+    </div>
+    <div class="children">
+
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+const props = defineProps({
+  icon: String,
+  label: {
+    type: String,
+    required: true
+  },
+  disabled: Boolean,
+  divider: {
+    type: Boolean,
+    default: false
+  }
+})
+
+onMounted(() => {
+  console.log('=== props ===', props)
+})
+</script>
